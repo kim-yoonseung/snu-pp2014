f56420e788d701fc43e0f27d88f537fc067066fd
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2014-09-01 19:30:24 +0900

    Add material for 20140903.

---
+++ lab/helloworld.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+; Provide for other module.
+(provide helloWorld)
+
+; Define the helloWorld function.
+(define (helloWorld)
+    "Hello, World!\n")
+
+; Uncomment the following line and see what happens.
+; Comment it before submission.
+; (printf (helloWorld))
