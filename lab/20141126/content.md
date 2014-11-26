da3df5527a35b49e07dd350e02763414314e3bf4
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2014-11-26 13:26:13 +0900

    Add 20141126's material.

---
+++ lab/20141126/content.md
@@ -0,0 +1,19 @@
+# Lab Session, Principles of Programming #
+
+20141126 (Wed) 16:00-17:50
+TA [Jeehoon Kang](http://sf.snu.ac.kr/~jeehoon.kang),
+[Yoonseung Kim](http://sf.snu.ac.kr/~yoonseung.kim)
+
+We are working on an inductive refinement example in OCaml.
+
+## Design ##
+
+See this [article](http://ropas.snu.ac.kr/~kwang/4190.210/12/inductive-refinement-eg.pdf).
+
+## Implementation ##
+
+Fill in the [skeleton](skeleton.ml).
+
+## Note: OCaml Programming ##
+
+See the [previous lab note](../20141119/content.md).
---
+++ lab/20141126/skeleton.ml
@@ -0,0 +1,24 @@
+exception TODO
+
+type matcher =
+  | Empty
+  | Char of int
+  | Append of (matcher * matcher)
+  | Union of (matcher * matcher)
+  | Star of matcher
+
+type string = int list
+
+(* See Figure 1 (bottom) *)
+let rec dagger (m:matcher) (char:int) : matcher =
+  match m with
+  | Empty -> Empty
+  | _ -> raise TODO
+
+(* See Figure 1 (top) *)
+let rec match_string (m:matcher) (s:string) : bool =
+  match m with
+  | Empty -> s = []
+  | _ -> raise TODO
+
+let _ = print_endline (string_of_bool (match_string (Append (Char 3, Char 4)) [4])) (* true *)
